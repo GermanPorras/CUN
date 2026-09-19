0e23c6be8bdbd2618d6771588e8aae35bcaa5c9b
unknown <german.porras@globalseis.com>
2026-09-19 11:29:59 -0500

    New test commit

--- src/Product.java
+++ src/Product.java
@@ -3,14 +3,17 @@ public class Product {
     private String name;
     private double price;
     private int quantity;
-//  Constructor de la clase que permitira asignar las variables al crear el objeto
-    public Product(String name, double price, int quantity){
+
+    // Constructor de la clase que permitira asignar las variables al crear el
+    // objeto
+    public Product(String name, double price, int quantity) {
         this.name = name;
         this.price = price;
         this.quantity = quantity;
     }
-//  Metodo para calcular y devolver el valor total de la venta actual
-    public double calcVenta(){
+
+    // Metodo para calcular y devolver el valor total de la venta actual
+    public double calcVenta() {
         return this.price * this.quantity;
     }
 }
